5710b8b9eec3442cb9054bb59591e06cc9628ca0
Sebastián Rivera Gutiérrez <riveragsebastian14@gmail.com>
2026-03-24 11:13:51 -0700

    Definimos las rutas.

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
+// Landing page.
 Route::get('/', function () {
-    return view('welcome');
+    return view('landingpage');
 });
+
+// Dashboard protegido.
+Route::get('/dashboard', function() {
+    return view('dashboard');
+})->middleware('auth');
+
+// Autenticación.
+Route::get('/login', [AuthController::class, 'showLogin']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::get('/register', [AuthController::class, 'showRegister']);
+Route::post('/register', [AuthController::class, 'register']);
+
+Route::post('/logout', [AuthController::class, 'logout']);
